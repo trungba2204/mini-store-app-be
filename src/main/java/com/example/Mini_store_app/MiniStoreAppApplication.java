3bd524ca938e999b9a57b2b345080ec8a3c1bc23
quandne092004 <quannguyen092004@gmail.com>
2025-07-15 09:18:00 +0700

    Quanden update

---
+++ src/main/java/com/example/Mini_store_app/MiniStoreAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mini_store_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniStoreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniStoreAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Mini_store_app/MiniStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mini_store_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniStoreAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
